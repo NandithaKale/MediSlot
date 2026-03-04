56b42c00a67ff8d405d70ab53c813705da1474ad
NandithaKale <nanditakale64@gmail.com>
2026-03-05 00:44:11 +0530

    Add UserRepository interface for user data access

---
+++ src/main/java/com/nanditha/medislot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nanditha.medislot.repository;
+
+import com.nanditha.medislot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
