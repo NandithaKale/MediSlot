6f370669d812dcfab0a4ecea1d35a7d2fe1b7ec8
NandithaKale <nanditakale64@gmail.com>
2026-03-05 01:07:03 +0530

    Initialize MedislotApplication with Spring Boot

---
+++ src/main/java/com/nanditha/medislot/MedislotApplication.java
@@ -0,0 +1,11 @@
+package com.nanditha.medislot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedislotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedislotApplication.class, args);
+    }
+}
