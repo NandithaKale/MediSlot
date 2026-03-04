1068edca890c190035c787ed23ac4b1241a3cdf2
NandithaKale <nanditakale64@gmail.com>
2026-03-05 00:33:14 +0530

    Create User.java

---
+++ src/main/java/com/nanditha/medislot/model/User.java
@@ -0,0 +1,31 @@
+package com.nanditha.medislot.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        ROLE_PATIENT, ROLE_DOCTOR, ROLE_ADMIN
+    }
+}
